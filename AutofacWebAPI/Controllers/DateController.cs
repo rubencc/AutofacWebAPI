d94de283be48269cdad07454196ae321d85d8b16
Rubén C.C. <ruben.carretero@wolterskluwer.com>
2017-04-17 21:17:21 +0200

    Esqueleto

---
+++ AutofacWebAPI/Controllers/DateController.cs
@@ -0,0 +1,18 @@
+﻿namespace AutofacWebAPI.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+
+    [RoutePrefix("api/date")]
+    public class DateController : ApiController
+    {
+        [HttpGet, Route("")]
+        public async Task<IHttpActionResult> GetDate()
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+            return Ok(DateTime.Now);
+        }
+    }
+}
--- AutofacWebAPI/Global.asax.cs
+++ AutofacWebAPI/Global.asax.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using Newtonsoft.Json.Serialization;
 
 namespace AutofacWebAPI
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Routing;
+    using Autofac;
+    using Autofac.Integration.WebApi;
+
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+            config.Formatters.RemoveAt(1);
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+
         }
     }
 }
